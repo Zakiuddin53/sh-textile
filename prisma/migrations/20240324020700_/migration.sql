d214516d2e995a55a4d97b63139d8e63b4cc45c4
Zakiuddin53 <zakiuddin810@gmail.com>
2024-11-12 16:24:30 +0530

    clean up

--- prisma/migrations/20240324020700_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `waistToheel` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "waistToheel",
-ADD COLUMN     "waistToHeel" DOUBLE PRECISION;
